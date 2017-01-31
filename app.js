d7d1661933caacb8f755caa48632425561cda18d
souvik zaminder <s.zaminder@gmail.com>
2017-01-31 14:20:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+function ViewModel() = {
+	this.availablelocations = [
+          {name:'Eden Gardens'}, {name :'Dumdum Airport'}, {name :'Victoria Memorial'}, {name : 'Prinsep Ghat'}, {name : 'Jorasanko Thakurbari'}, {name : 'Dakshineswar Kali Temple'}];
+    this.chosenlocation = ko.observable();
+    this.resetlocation = function(){this.chosenlocation(null)};
+    };
+    ko.applyBindings(new ViewModel() );
