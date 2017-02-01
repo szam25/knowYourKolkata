f3ae2c7d849ed87792d9fae85f4c55ca57f47d12
souvik zaminder <s.zaminder@gmail.com>
2017-02-01 17:54:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $("#menu-toggle")
     .click(function(e) {
         //e.preventDefault();
         $("#wrapper")
-            .toggleClass("toggle");
+            .toggleClass("toggled");
     });
 var markers = [];
 var locations = [
